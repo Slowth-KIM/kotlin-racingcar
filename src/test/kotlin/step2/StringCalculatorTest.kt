07a14fb9e8b83142e5accb65b62f2073c15c7841
Slowth-KIM <rt.slowth@gmail.com>
2024-12-02 22:10:06 +0900

    test failed 기본 사칙 연산

---
+++ src/test/kotlin/step2/StringCalculatorTest.kt
@@ -0,0 +1,28 @@
+package step2
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+
+class StringCalculatorTest : FunSpec({
+	val calculator = StringCalculator()
+
+	context("기본 사칙 연산") {
+		test("덧셈 연산을 수행한다") {
+			calculator.calculate("2 + 3") shouldBe 5
+		}
+
+		test("뺄셈 연산을 수행한다") {
+			calculator.calculate("5 - 3") shouldBe 2
+		}
+
+		test("곱셈 연산을 수행한다") {
+			calculator.calculate("4 * 3") shouldBe 12
+		}
+
+		test("나눗셈 연산을 수행한다") {
+			calculator.calculate("8 / 2") shouldBe 4
+		}
+	}
+
+})
